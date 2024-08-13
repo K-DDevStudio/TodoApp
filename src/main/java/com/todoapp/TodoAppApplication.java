4023c643229d24a5ae4c598e5190572ed6fd7358
Karyna Yarmosh <karyar@st.amu.edu.pl>
2024-08-13 18:07:26 +0200

    SCRUM-18 Create Event Entity

--- src/main/java/com/todoapp/TodoAppApplication.java
+++ src/main/java/com/todoapp/TodoAppApplication.java
@@ -1,12 +1,26 @@
 package com.todoapp;
 
+import entities.Event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class TodoAppApplication {
 
     public static void main(String[] args) {
+
+        Event event = Event.builder()
+                        .id(1L)
+                        .title("A")
+                        .description("B")
+                        .startDateTime(LocalDateTime.of(2024, 9, 12, 9, 0))
+                        .endDateTime(LocalDateTime.of(2024, 9, 12, 17, 0))
+                        .build();
+
+        System.out.println(event);
+
         SpringApplication.run(TodoAppApplication.class, args);
     }
 
