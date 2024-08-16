9c1db17de3b6ebc89e20d06a5292d1ab26819a23
Karyna Yarmosh <karyar@st.amu.edu.pl>
2024-08-16 23:46:19 +0200

    SCRUM-36 stast testing application

---
+++ src/test/java/com/todoapp/controller/EventControllerTest.java
@@ -0,0 +1,4 @@
+package com.todoapp.controller;
+
+public class EventControllerTest {
+}
---
+++ src/test/java/com/todoapp/service/EventServiceTest.java
@@ -0,0 +1,52 @@
+package com.todoapp.service;
+
+import com.todoapp.controller.EventController;
+import com.todoapp.entities.Event;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventServiceTest {
+
+    //@Mock используется для создания имитации объекта EventService.
+    @Mock
+    private EventService eventService;
+
+    //Аннотация @InjectMocks внедряет все зависимости в объект EventController.
+    @InjectMocks
+    private EventController eventController;
+
+    @Test
+    public void testGetAllEvents() {
+        // Arrange
+        Event event1 = new Event();
+
+        event1.setId(1L);
+        event1.setTitle("First Event");
+        event1.setDescription("Description for the first event");
+        event1.setStartDateTime(LocalDateTime.now().plusDays(1));
+        event1.setEndDateTime(LocalDateTime.now().plusDays(2));
+
+        when(eventService.getAllEvents()).thenReturn(singletonList(event1));
+
+        ResponseEntity<List<Event>> response = eventController.getAll();
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(event1, response.getBody().get(0));
+
+    }
+}
