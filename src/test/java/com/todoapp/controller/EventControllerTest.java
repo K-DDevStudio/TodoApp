a2ce8ac01b2ab0df418fd9a57af6f722779a4b52
Karyna Yarmosh <karyar@st.amu.edu.pl>
2024-08-18 23:01:20 +0200

    SCRUM-36 end testing controller

--- src/test/java/com/todoapp/controller/EventControllerTest.java
+++ src/test/java/com/todoapp/controller/EventControllerTest.java
@@ -1,6 +1,7 @@
 package com.todoapp.controller;
 
 import com.todoapp.TestDataForEvents;
+import com.todoapp.TodoAppApplication;
 import com.todoapp.entities.Event;
 import com.todoapp.repository.EventRepository;
 import com.todoapp.service.EventService;
@@ -12,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -63,6 +65,56 @@ public class EventControllerTest {
 
         assertEquals(ResponseEntity.noContent().build(), response);
 
+    }
+
+    @Test
+    public void testCreateEvent() {
+
+        Event eventToCreate = new Event();
+        eventToCreate.setTitle("New Event");
+        eventToCreate.setDescription("Event Description");
+        eventToCreate.setStartDateTime(LocalDateTime.now().plusDays(1));
+        eventToCreate.setEndDateTime(LocalDateTime.now().plusDays(2));
+
+        Event savedEvent = new Event();
+        savedEvent.setId(3L);
+        savedEvent.setTitle("New Event");
+        savedEvent.setDescription("Event Description");
+        savedEvent.setStartDateTime(LocalDateTime.now().plusDays(1));
+        savedEvent.setEndDateTime(LocalDateTime.now().plusDays(2));
+
+        when(eventService.saveEvent(eventToCreate)).thenReturn(savedEvent);
+
+        ResponseEntity<Event> response = eventController.createEvent(eventToCreate);
+
+        assertEquals(savedEvent, response.getBody());
+
+    }
+
+    @Test
+    public void testUpdateEvent() {
+        List<Event> mockEvents = TestDataForEvents.getMockEvents();
+
+        Event existingEventWithId1L = mockEvents.stream()
+                .filter(event -> event.getId().equals(1L))
+                .findFirst()
+                .orElse(null);
+
+        // The updated event data
+        Event updatedEventData = new Event();
+        updatedEventData.setId(1L);
+        updatedEventData.setTitle("Updated Event");
+        updatedEventData.setDescription("Updated Description");
+        updatedEventData.setStartDateTime(LocalDateTime.now().plusDays(3));
+        updatedEventData.setEndDateTime(LocalDateTime.now().plusDays(5));
+
+        when(eventService.updateEvent(updatedEventData, 1L)).thenReturn(updatedEventData);
+
+        ResponseEntity<Event> response = eventController.updateEvent(updatedEventData, 1L);
+
+        assertEquals(ResponseEntity.ok(updatedEventData), response);
+        assertEquals("Updated Event", response.getBody().getTitle());
+        assertEquals("Updated Description", response.getBody().getDescription());
     }
 
 }
